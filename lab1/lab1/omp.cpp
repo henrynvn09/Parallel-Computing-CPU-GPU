436cef08ed6244654f0b03e7c919326562f9b28c
henrynvn09 <henrynvn09@gmail.com>
2025-04-13 18:02:33 -0700

    lab 1: part 1 80 GFlops

--- lab1/lab1/omp.cpp
+++ lab1/lab1/omp.cpp
@@ -6,5 +6,19 @@
 
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
-  // Your code goes here...
+  //TODO: Your code goes here...
+
+  #pragma omp parallel for
+  for (int i = 0; i < kI; ++i) {
+    std::memset(c[i], 0, sizeof(float) * kJ);
+  }
+
+  #pragma omp parallel for shared(a, b, c)
+  for (int i = 0; i < kI; ++i) {
+    for (int k = 0; k < kK; ++k) {
+      for (int j = 0; j < kJ; ++j) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
 }
