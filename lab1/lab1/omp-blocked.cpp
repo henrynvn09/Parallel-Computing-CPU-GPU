b89f9768d7ee7a56bf603cf9b3e8fd7df29d32ec
henrynvn09 <henrynvn09@gmail.com>
2025-04-15 06:05:30 +0000

    lab1: 48 GFLOPS

--- lab1/lab1/omp-blocked.cpp
+++ lab1/lab1/omp-blocked.cpp
@@ -15,24 +15,25 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   }
 
   int blockSizeI = 64;
-  int blockSizeK = 16;
-  int blockSizeJ = 128;
-  int i, k, j, bi, bk, bj;
-  
+  int blockSizeK = 64;
+  int blockSizeJ = 64;
+  int i, k, j, bi, bk, bj;  
 
   #pragma omp parallel for shared(a, b, c) private(i, k, j, bi, bk, bj)
   for (bi = 0; bi < kI; bi += blockSizeI) {
     for (bk = 0; bk < kK; bk += blockSizeK) {
+      const int iMax = std::min(bi + blockSizeI, kI);
+      const int kMax = std::min(bk + blockSizeK, kK);
       for (bj = 0; bj < kJ; bj += blockSizeJ) {
-        for (i = bi; i < bi + blockSizeI && i < kI; ++i) {
-          for (k = bk; k < bk + blockSizeK && k < kK; ++k) {
-            for (j = bj; j < bj + blockSizeJ && k < kJ; ++j) {
-                c[i][j] += a[i][k] * b[k][j];
-              }
+        for (i = bi; i < iMax; ++i) {
+          for (k = bk; k < kMax; ++k) {
+            for (j = bj; j < std::min(bj + blockSizeJ, kJ); ++j) {
+              c[i][j] += a[i][k] * b[k][j];
             }
+          }
         }
       }
     }
-  }
 
 }
+                         }
