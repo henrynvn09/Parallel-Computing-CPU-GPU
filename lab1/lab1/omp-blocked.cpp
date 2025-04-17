9b374add4665cb26dad14a7f0389a9f9cf51b8f5
henrynvn09 <henrynvn09@gmail.com>
2025-04-17 04:00:36 +0000

    lab1: add brute-force + small tile to incentive caching

--- lab1/lab1/omp-blocked.cpp
+++ lab1/lab1/omp-blocked.cpp
@@ -1,3 +1,7 @@
+#define TILE_I 256
+#define TILE_J 512
+#define TILE_K 8
+
 // Header inclusions, if any...
 
 #include "lib/gemm.h"
@@ -5,35 +9,48 @@
 
 // Using declarations, if any...
 
-void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
-                         float c[kI][kJ]) {
-  //TODO: Your code goes here...
+void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ], float c[kI][kJ]) {
+    float tileA[TILE_I][TILE_K], tileB[TILE_K][TILE_J];
+    int i, j, k;
 
-  #pragma omp parallel for
-  for (int i = 0; i < kI; ++i) {
-    std::memset(c[i], 0, sizeof(float) * kJ);
-  }
-
-  int blockSizeI = 64;
-  int blockSizeK = 64;
-  int blockSizeJ = 64;
-  int i, k, j, bi, bk, bj;  
-
-  #pragma omp parallel for shared(a, b, c) private(i, k, j, bi, bk, bj)
-  for (bi = 0; bi < kI; bi += blockSizeI) {
-    for (bk = 0; bk < kK; bk += blockSizeK) {
-      const int iMax = std::min(bi + blockSizeI, kI);
-      const int kMax = std::min(bk + blockSizeK, kK);
-      for (bj = 0; bj < kJ; bj += blockSizeJ) {
-        for (i = bi; i < iMax; ++i) {
-          for (k = bk; k < kMax; ++k) {
-            for (j = bj; j < std::min(bj + blockSizeJ, kJ); ++j) {
-              c[i][j] += a[i][k] * b[k][j];
-            }
-          }
-        }
-      }
+    #pragma omp parallel for private(i)
+    for (i = 0; i < kI; ++i) {
+        std::memset(c[i], 0, sizeof(float) * kJ);
     }
 
+    #pragma omp parallel for private(tileA, tileB, i, j, k)
+    for (int ib = 0; ib < kI; ib += TILE_I)
+    {
+        for (int jb = 0; jb < kJ; jb += TILE_J) {
+            float tileC[TILE_I][TILE_J] = {0};
+
+            for (int kb = 0; kb < kK; kb += TILE_K) {
+                for (i = 0; i < TILE_I; ++i) {
+                    for (k = 0; k < TILE_K; ++k) {
+                        tileA[i][k] = a[ib + i][kb + k];
+                    }
+                }
+
+                for (k = 0; k < TILE_K; ++k) {
+                    for (j = 0; j < TILE_J; ++j) {
+                        tileB[k][j] = b[kb + k][jb + j];
+                    }
+                }
+
+                for (i = 0; i < TILE_I; ++i) {
+                    for (j = 0; j < TILE_J; ++j) {
+                        for (k = 0; k < TILE_K; ++k) {
+                            tileC[i][j] += tileA[i][k] * tileB[k][j];
+                        }
+                    }
+                }
+            }
+
+            for (i = 0; i < TILE_I; ++i) {
+                for (j = 0; j < TILE_J; ++j) {
+                    c[ib + i][jb + j] += tileC[i][j];
+                }
+            }
+        }
+    }
 }
-                         }
